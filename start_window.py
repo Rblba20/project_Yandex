4423456ad78d9e38b5f9b42969e375f234f33635
Rblba20 <hp5722*2005ML>
2020-12-25 20:02:32 +0500

    Переделан стартовый экран

--- start_window.py
+++ start_window.py
@@ -1,5 +1,5 @@
 import os
-import random
+import sys
 
 import pygame
 
@@ -22,68 +22,52 @@ def load_image(name, colorkey=None):
     return image
 
 
-all_sprites = pygame.sprite.Group()
-name = pygame.sprite.Sprite()
-name.image = load_image("name_game.png")
-name.rect = name.image.get_rect()
-all_sprites.add(name)
-name.rect.x = 20
-name.rect.y = 0
-start_image = load_image("start.png")
-start = pygame.sprite.Sprite(all_sprites)
-start.image = start_image
-start.rect = start.image.get_rect()
-start.rect.x = -87
-start.rect.y = 125
-results_image = load_image("results.png")
-results = pygame.sprite.Sprite(all_sprites)
-results.image = results_image
-results.rect = results.image.get_rect()
-results.rect.x = 50
-results.rect.y = 200
-exit_image = load_image("exit.png")
-exit = pygame.sprite.Sprite(all_sprites)
-exit.image = exit_image
-exit.rect = exit.image.get_rect()
-exit.rect.x = 50
-exit.rect.y = 260
-copyrights_image = load_image("copyrights.png")
-copyrights = pygame.sprite.Sprite(all_sprites)
-copyrights.image = copyrights_image
-copyrights.rect = copyrights.image.get_rect()
-copyrights.rect.x = 30
-copyrights.rect.y = 500
+FPS = 50
 
-running = True
-i = 0
-stars = []
-while running:
-    screen.fill(pygame.Color('black'))
-    if i == 0:
-        for i in range(5000):
-            a = random.random() * width
-            b = random.random() * height
-            screen.fill(pygame.Color('white'),
-                        (a,
-                         b, 1, 1))
-            stars.append([a, b])
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ["  Star Fighter", "",
+                  "[Z]Играть",
+                  "[S]Результаты",
+                  "[X]Выход", ""]
+    rights = ["© Made by Rblba20 and lapin01", "",
+              "All rights reserved"]
+
+    fon = pygame.transform.scale(load_image('fon.jpg'), (width, height))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font('data/text.ttf', 70)
+    font_ = pygame.font.Font('data/text.ttf', 35)
+    text_coord = 50
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    for line in rights:
+        string_rendered = font_.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+    clock = pygame.time.Clock()
+    while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                running = False
-            all_sprites.draw(screen)
-            pygame.display.flip()
-    else:
-        for i in range(5000):
-            screen.fill(pygame.Color('white'),
-                        (stars[i][0],
-                         stars[i][1], 1, 1))
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+                terminate()
             if event.type == pygame.KEYDOWN and event.key == pygame.K_z:
-                pass
-            all_sprites.draw(screen)
-            all_sprites.update(event)
-            pygame.display.flip()
-    i += 1
-pygame.quit()
+                return
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+start_screen()
